4e20f2a0c34cb7ca5fed2d5e82a4a5bb05155428
Rupika Pawar <rupikapawar3197@gmail.com>
2019-03-30 15:07:43 +0530

    Farmers Signup layout and Buyers layout margin change

--- app/src/main/java/com/example/rupik/veggarden/FarmerSignupActivity.java
+++ app/src/main/java/com/example/rupik/veggarden/FarmerSignupActivity.java
@@ -2,13 +2,25 @@ package com.example.rupik.veggarden;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FarmerSignupActivity extends AppCompatActivity {
 
+    EditText mName, mEmail, mContact, mAadhar, mPass, mConfirmPass;
+    Button mSignup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farmer_signup);
+        mName = findViewById(R.id.fsignname);
+        mEmail = findViewById(R.id.fsignemail);
+        mContact = findViewById(R.id.fsigncontact);
+        mAadhar = findViewById(R.id.fsignadhar);
+        mPass = findViewById(R.id.fsignpass);
+        mConfirmPass = findViewById(R.id.fsigncpass);
+        mSignup = findViewById(R.id.fsignupbtn);
+
 
     }
 }
