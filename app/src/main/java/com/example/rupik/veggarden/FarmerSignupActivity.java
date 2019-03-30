7dd3cf2292c5445b8384169bcfee4283a27122d7
Nishant <nishntsa97@gmail.com>
2019-03-30 13:40:08 +0530

    Making MainActivity partially working and created FarmerSignupActivity(empty)

---
+++ app/src/main/java/com/example/rupik/veggarden/FarmerSignupActivity.java
@@ -0,0 +1,14 @@
+package com.example.rupik.veggarden;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FarmerSignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_farmer_signup);
+
+    }
+}
